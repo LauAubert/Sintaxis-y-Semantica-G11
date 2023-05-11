e3487368a858a768e7ae378e3fe3f6f1030abf0a
Valentin Torres <valentintorres@alu.frlp.utn.edu.ar>
2023-05-10 22:14:04 -0300

    Tad Proceso comentado

--- TadProceso.py
+++ TadProceso.py
@@ -37,47 +37,61 @@ def cargar_proceso(proc,pid:int,nom:str,tipo:str,tam:int,prio:int,minutos:int,ho
 
 
 def mod_nom(proc,nom):
+    #Modifica el nombre del proceso
     proc[1] = nom
 
 def mod_tipo(proc,tipo):
+    #Modifica el tipo del proceso
     proc[2] = tipo
 
 def mod_tam(proc,tam):
+    #Modifica el tamano del proceso
     proc[3] = tam
 
 def mod_prio(proc,prio):
+    #Modifica la prioridad del proceso
     proc[4] = prio
 
 def ver_pid(proc):
+    #Devuelve el id del proceso
     return (proc[0])
 
 def ver_nom(proc):
+    #Devuelve el nombre del proceso
     return (proc[1])
 
 def ver_tipo(proc):
+    #Devuelve el tipo del proceso
     return (proc[2])
 
 def ver_tam(proc):
+    #Devuelve el tamano del proceso
     return (proc[3])
 
 def ver_prio(proc):
+    #Devuelve la prioridad del proceso
     return (proc[4])
 
 def copiar(destino,origen):
+    #Copia los datos de un proceso a otro
     mod_nom(destino,ver_nom(origen))
     mod_prio(destino,ver_prio(origen))
     mod_tam(destino,ver_tam(origen))
     mod_tipo(destino,ver_tipo(origen))
-    destino[5] = datetime.datetime.now()
+    destino[5] = origen[5]
 
 def ver_fecha(proc):
+    #Devuelve la fecha del proceso en formato dd-mm hh:mm
     return proc[5].strftime("%d-%m %H:%M")
 
 def ver_min(proc):
+    #Devuelve los minutos de la fecha del proceso
     return proc[5].minute
 
 def ver_hora(proc):
+    #Devuelve la hora de la fecha del proceso
     return proc[5].hour
 
 def ver_mes(proc):
+    #Devuelve el mes de la fecha del proceso
     return proc[5].month
